677605f2b9347af09759c355010ef7d2fcac6150
Aleksandr <shaban4@yandex.ru>
2024-09-30 15:49:30 +0700

    add 2-9 page

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -106,7 +106,6 @@ function initSliders() {
 			// }
 		});
 	}
-
 	if (document.querySelector('.popular-servises__slider')) { // Указываем скласс нужного слайдера
 		// Создаем слайдер
 		new Swiper('.popular-servises__slider', { // Указываем скласс нужного слайдера
@@ -242,6 +241,80 @@ function initSliders() {
 					spaceBetween: 77,
 				},
 			},
+
+			// События
+			// on: {
+
+			// }
+		});
+	}
+	if (document.querySelector('.examples__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.examples__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation],
+			observer: true,
+			observeParents: true,
+			slidesPerView: "auto",
+			spaceBetween: 6,
+			watchSlidesProgress: true,
+			speed: 800,
+			centeredSlides: true,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			loop: true,
+			// autoHeight: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+			/*
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			*/
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.examples__slider .swiper-button-prev',
+				nextEl: '.examples__slider .swiper-button-next',
+			},
+
+			// Брейкпоинты
+
+			// breakpoints: {
+			// 	320: {
+			// 		slidesPerView: 1,
+			// 		spaceBetween: 25,
+			// 	},
+			// 	1199.98: {
+			// 		slidesPerView: 2,
+			// 		spaceBetween: 25,
+			// 	},
+			// 	1399.98: {
+			// 		slidesPerView: 2,
+			// 		spaceBetween: 77,
+			// 	},
+			// },
 
 			// События
 			// on: {
